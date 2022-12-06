1396079b8493e9a9268b0185fb2f6ae0d76a8c4f
Kirstin Lyon <93132914+KirstinCLyon@users.noreply.github.com>
2022-12-06 14:56:07 +0100

    Add files via upload

---
+++ Data Dictionary.R
@@ -0,0 +1,25 @@
+
+# Instructions for preparing Tableau File ----
+# Unpack to get the twb
+# rename the twb to an xml
+
+
+# Libraries ----
+
+library(tidyverse)
+library(glamr) #for setting up the USAID way
+
+
+
+
+# Folder structure ----
+folder_setup(
+    folder_list = list("Data", "Images", "Scripts", "AI", "Dataout", "GIS", "Documents",
+                       "Graphics", "markdown", "Tableau")
+)
+
+
+# Global Variables in all caps ----
+
+
+# Read data ----
